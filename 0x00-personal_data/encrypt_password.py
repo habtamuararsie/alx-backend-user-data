60ce1eeba60509966d2d26d4107c9c4635a3450b
unknown <habtamuararsie@yahoo.com>
2023-04-27 15:50:42 +0300

    filtere logger

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""[Module]"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """[summary]
+    Args:
+        password (str): [description]
+    Returns:
+        bytes: [description]
+    """
+    return bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """[summary]
+    Args:
+        hashed_password (bytes): [description]
+        password (str): [description]
+    Returns:
+        bool: [description]
+    """
+    return bool(bcrypt.checkpw(str.encode(password), hashed_password))
